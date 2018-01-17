f1b6f5b3b59c357b2d8b9578879bf1af39e704b9
Terry.FromMac <tkstorm@163.com>
2018-01-18 01:51:52 +0800

    App Jwt Demo Test .

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,28 +3,42 @@
 namespace App\Providers;
 
 use App\Account;
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Dingo\Api\Auth\Provider\Authorization;
+use Dingo\Api\Routing\Route;
+use Illuminate\Http\Request;
+use Dingo\Api\Contract\Auth\Provider as ServiceProvider;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
-class AuthServiceProvider extends ServiceProvider
-{
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+//class AuthServiceProvider implements ServiceProvider{
+class AuthServiceProvider extends Authorization {
 
     /**
-     * Register any authentication / authorization services.
+     * Get the providers authorization method.
      *
-     * @return void
+     * @return string
      */
-    public function boot()
+    public function getAuthorizationMethod()
     {
-        $this->registerPolicies();
+        // TODO: Implement getAuthorizationMethod() method.
 
     }
+
+    /**
+     * Authenticate the request and return the authenticated user instance.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Dingo\Api\Routing\Route $route
+     *
+     * @return mixed
+     */
+    public function authenticate(Request $request, Route $route)
+    {
+        // TODO: Implement authenticate() method.
+        if ($request->input('password') == '123456') {
+            return ['user' => 'Terry.clark'];
+        }
+
+        throw new UnauthorizedHttpException('Unable to authenticate with supplied username and password.');
+    }
 }
+
--- config/app.php
+++ config/app.php
@@ -172,12 +172,12 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
+//        App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
